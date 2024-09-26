858e0863ae66dd2393442686ff695298d54cd691
Samm Wiltshire <samm94tw@gmail.com>
2024-09-26 14:18:30 +0100

    chore: install cors

--- src/middleware/auth.js
+++ src/middleware/auth.js
@@ -24,6 +24,9 @@ const comparePass = async (req, res, next) => {
     try{
         // Step 1: find user using the username (req.body.username?)
 const user = await User.findOne ({where: {username: req.body.username}});
+        // if (!user) {
+        //     return res.status(404).json({message: "noone user", user: req.user.username});
+        // };
         //Step 2: compare the plaintext password with the hashed password on the DB
 const match = await bcrypt.compare(req.body.password, user.password);
         // Step 3: if false, send response "passwords do not match" - just if
